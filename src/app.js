7c2400cc3c33f4db36fbe663c740090fe58aa429
Siddhartha <sidskole@gmail.com>
2024-11-12 00:54:09 +0530

    handle custom error and api response

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
 const app = express()
+
+// use method is utilised in middleweares and configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))  // enabling cross origins from where requests will be sent to the server
+
+app.use(express.json({
+    limit: "16kb"
+})) // enabling json requests and setting a size limit
+
+app.use(express.urlencoded({
+    extended:true,
+    limit: "16kb"
+})) // enabling requests from urls with further extended objects
+
+app.use(express.static("public")) // enabling storage of public assets in public folder(the folder above src), for example: pdf, images
+
+app.use(cookieParser()) // accessing the cookies in user's browser from server and perform crud operatrions on it
 
 
 export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// we need to talk to the database repetitively and each time it is unconvenient to write out try catch wrapped medthods
+// hence we utilise this generalised function which wraps the code in a try catch block/ promise thereby avoiding repetition
+
+// Approach-1: a promise-resolve
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+// Approach-2: a try catch block
+/*
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
+
+export {asyncHandler}
