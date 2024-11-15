0c8fdd9d1864d35199cc9cb66d06b45a515a22c6
Siddhartha <sidskole@gmail.com>
2024-11-15 07:06:30 +0530

    add middleware and util

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,23 @@
+/*
+    1.User uploads file → Multer handles upload.
+    2.Multer stores file temporarily → Validates file.
+    3.Validated file → Uploaded to Cloudinary.
+    4.Cloudinary processes file → Returns URL.
+    5.URL stored in database → File served from Cloudinary CDN.
+    6Think of Multer as the "file handler" and Cloudinary as the "file host".
+ */
+
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "/public/temp")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({ 
+    storage 
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+// all the tasks related to file upload will be taken care of here
+// actually the user uploads a file from their device to the local server(front end) and then
+// the file is uploaded to cloudinary
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+// Upload an image
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const uploadResult = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        // file has been uploaded successfully
+        console.log("File uploaded successfully: ",uploadResult.url)
+        return uploadResult
+    }catch(error){
+        fs.unlinkSync(localFilePath) // remove the local temporary file as the upload failed
+        console.log(error)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
